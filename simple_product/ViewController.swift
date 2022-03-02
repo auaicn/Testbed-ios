f651a00709fcc2b4a0238f7a4217bff30454fddf
Kyung Ho, Park <crorowslowpp@gmail.com>
2022-03-03 04:33:36 +0900

    Initial Commit

---
+++ simple_product/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_product
+//
+//  Created by Kyungho on 2022/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
