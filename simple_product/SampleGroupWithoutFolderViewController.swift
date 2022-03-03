0ab2be6a3c06d8275f0990f1c34fc16a7c2b7d24
Kyung Ho, Park <crorowslowpp@gmail.com>
2022-03-03 19:11:10 +0900

    Feat: Added Group (with and without folder respectively)

---
+++ simple_product/SampleGroupWithoutFolderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SampleGroupWithoutFolderViewController.swift
+//  simple_product
+//
+//  Created by Kyungho on 2022/03/03.
+//
+
+import UIKit
+
+class SampleGroupWithoutFolderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
